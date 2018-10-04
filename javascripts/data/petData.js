67553cc3c8b30a3b7377b8e789bf8ac3f83210f0
Maggie Leavell <mjleavell@gmail.com>
2018-10-03 20:03:43 -0500

    added json data, initializeApp function

--- javascripts/data/petData.js
+++ javascripts/data/petData.js
@@ -1,1 +1,18 @@
-console.log('petData');
+function fileLoadSuccess () {
+    const data = JSON.parse(this.responseText);
+    console.log('request', data.pets);
+}
+
+function fileLoadError () {
+    console.log('things are broken');
+}
+
+const getPetData = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', fileLoadSuccess);
+    myRequest.addEventListener('error', fileLoadError);
+    myRequest.open('GET', './db/pets.json');
+    myRequest.send();
+}
+
+export {getPetData}
